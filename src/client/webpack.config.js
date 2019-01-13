0087bd55a2da7d10d55b22465b0e5295703d7fa4
Gil Amran <gil@orbs.com>
2019-01-13 16:25:21 +0200

    Upgraded all the external dependecies

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -1,36 +1,38 @@
-const path = require('path');
-const webpack = require('webpack');
-const version = require('../../package.json').version;
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
+const path = require("path");
+const version = require("../../package.json").version;
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const OpenBrowserPlugin = require("open-browser-webpack-plugin");
+const cssnano = require("cssnano");
 
-const config = require('../server/config');
-const projectRoot = path.join(__dirname, '..', '..');
+const config = require("../server/config");
+const projectRoot = path.join(__dirname, "..", "..");
 
 const plugins = [
   new HtmlWebpackPlugin({
-    title: 'TypeScript and React',
-    favicon: 'favicon.ico',
-    filename: 'index.html',
-    template: 'index.ejs'
+    title: "TypeScript and React",
+    favicon: "favicon.ico",
+    filename: "index.html",
+    template: "index.ejs"
   })
 ];
 
 if (!config.IS_PRODUCTION) {
-  plugins.push(new OpenBrowserPlugin({ url: `http://localhost:${config.SERVER_PORT}` }));
+  plugins.push(
+    new OpenBrowserPlugin({ url: `http://localhost:${config.SERVER_PORT}` })
+  );
 }
 
 module.exports = {
-  mode: config.IS_PRODUCTION ? 'production' : 'development',
-  devtool: config.IS_PRODUCTION ? '' : 'inline-source-map',
-  entry: ['babel-polyfill', './client'],
+  mode: config.IS_PRODUCTION ? "production" : "development",
+  devtool: config.IS_PRODUCTION ? "" : "inline-source-map",
+  entry: ["babel-polyfill", "./client"],
   output: {
-    path: path.join(projectRoot, 'dist', 'public'),
+    path: path.join(projectRoot, "dist", "public"),
     filename: `[name]-${version}-bundle.js`,
-    publicPath: '/public/'
+    publicPath: "/public/"
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".js", ".ts", ".tsx"]
   },
   optimization: {
     splitChunks: {
@@ -47,28 +49,34 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'awesome-typescript-loader'
+        use: "awesome-typescript-loader"
       },
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: "style-loader"
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               modules: true,
               camelCase: true,
+              sourceMap: !config.IS_PRODUCTION
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
               sourceMap: !config.IS_PRODUCTION,
-              minimize: config.IS_PRODUCTION
+              plugins: config.IS_PRODUCTION ? [] : [cssnano()]
             }
           }
         ]
       },
       {
         test: /.jpe?g$|.gif$|.png$|.svg$|.woff$|.woff2$|.ttf$|.eot$/,
-        use: 'url-loader?limit=10000'
+        use: "url-loader?limit=10000"
       }
     ]
   },
