5985aef4e84b7f34035cd46935bc48c2bbb8dbea
Gil Amran <gilamran@gmail.com>
2019-12-16 22:54:12 +0200

    Upgraded to the latest and greatest

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,4 +1,4 @@
-import Grid from '@material-ui/core/Grid';
+import { CssBaseline, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { About } from './components/About';
@@ -7,15 +7,26 @@ import { Header } from './components/Header';
 import { Home } from './components/Home';
 import { UsersList } from './components/UsersList';
 
-export const App = () => (
-  <BrowserRouter>
-    <Grid container spacing={2}>
+const useStyles = makeStyles(() => ({
+  main: {
+    margin: 8
+  },
+}));
+
+export const App = () => {
+  const classes = useStyles({});
+
+  return (
+    <BrowserRouter>
+      <CssBaseline />
       <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/users-list' component={UsersList} />
-      </Switch>
-    </Grid>
-  </BrowserRouter>
-);
+      <main className={classes.main}>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/users-list' component={UsersList} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
+};
