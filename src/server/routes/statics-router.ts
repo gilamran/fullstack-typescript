437d3dd9fe5cddb296015119a33c3e77b6d4b29f
Gil Amran <gilamran@gmail.com>
2020-05-13 22:17:06 +0300

    Upgraded to the latest versions

--- src/server/routes/statics-router.ts
+++ src/server/routes/statics-router.ts
@@ -2,17 +2,17 @@ import path from 'path';
 import express from 'express';
 import { Router } from 'express';
 import { IS_DEV, WEBPACK_PORT } from '../config';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export function staticsRouter() {
   const router = Router();
 
   if (IS_DEV) {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const proxy = require('http-proxy-middleware');
     // All the assets are hosted by Webpack on localhost:${config.WEBPACK_PORT} (Webpack-dev-server)
     router.use(
       '/statics',
-      proxy({
+      createProxyMiddleware({
         target: `http://localhost:${WEBPACK_PORT}/`,
       }),
     );
