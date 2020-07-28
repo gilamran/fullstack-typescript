8bbe18c7e7fca3c3e7d7ed3cd487ebbad3722084
Gil Amran <gilamran@gmail.com>
2020-07-28 21:15:29 +0300

    require http-proxy-middleware only on dev

--- src/server/routes/statics-router.ts
+++ src/server/routes/statics-router.ts
@@ -2,12 +2,12 @@ import path from 'path';
 import express from 'express';
 import { Router } from 'express';
 import { IS_DEV, WEBPACK_PORT } from '../config';
-import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export function staticsRouter() {
   const router = Router();
-
+  
   if (IS_DEV) {
+    const { createProxyMiddleware } = require('http-proxy-middleware');
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     // All the assets are hosted by Webpack on localhost:${config.WEBPACK_PORT} (Webpack-dev-server)
     router.use(
