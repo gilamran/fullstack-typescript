fe91f7fe51d05e9749f5bd94f178ab9eaa36926c
Gil Amran <gilamran@gmail.com>
2025-04-20 13:15:33 +0300

    upgraded to the latest versions

--- src/server/routes/pages-router.ts
+++ src/server/routes/pages-router.ts
@@ -5,7 +5,7 @@ import { IS_DEV } from '../config';
 export function pagesRouter(): Router {
   const router = Router();
 
-  router.get(`/**`, async (_, res) => {
+  router.get(`/{*path}`, async (_, res) => {
     const manifest = await getManifest();
     const renderData = { IS_DEV, manifest };
     res.render('page.ejs', renderData);
