4d4ec239274fb61c963a60b10dbef59e8326c2a5
Gil Amran <gilamran@gmail.com>
2026-06-05 10:58:59 +0300

    removed axios

--- src/client/utils/api-facade.ts
+++ src/client/utils/api-facade.ts
@@ -1,6 +1,10 @@
-import axios from 'axios';
 import type { IUserDTO } from '@shared/IUserDTO';
 
 export function loadUsersAPI() {
-  return axios.get(`/api/users`).then((res) => res.data as IUserDTO[]);
+  return fetch('/api/users').then((response) => {
+    if (!response.ok) {
+      throw new Error(`Failed to load users: ${response.status}`);
+    }
+    return response.json() as Promise<IUserDTO[]>;
+  });
 }
